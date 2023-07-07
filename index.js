fbde534f2f34d44ae24f6fce459e0183045f515c
VanajaGurram <138871711+VanajaGurram@users.noreply.github.com>
2023-07-07 16:09:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var clickSound=new Audio()
+var clickSound1=new Audio()
+clickSound.src="./click1.mp3"
+clickSound1.src="./click.mp3"
+function clickAudio()
+{
+    clickSound.play()
+}
+function clickAudio1()
+{
+    clickSound.play()
+}
